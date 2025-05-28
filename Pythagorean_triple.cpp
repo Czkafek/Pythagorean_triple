52b78cdd7fc2c1efac7807139a159ceeba1a52a8
Czkafek <czkafek@gmail.com>
2025-05-28 19:21:06 +0200

    Algorythm is done

--- Pythagorean_triple.cpp
+++ Pythagorean_triple.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <cmath>
+#include <math.h>
 using namespace std;
+
+int nwd(int a, int b) {
+	while (a != b)
+	{
+		if (a > b) a -= b;
+		else b -= a;
+	}
+	return a;
+}
 
 int main()
 {
@@ -14,22 +24,50 @@ int main()
 	cout << "Podaj koniec przedziału: ";
 	cin >> stop;
 
-	for (int i = start; i < stop + 1; i++) {
-		long long int sqrI = 1LL * i * i;
-		for (int j = i; j < stop + 1; j++) {
-			long long sum = sqrI + 1LL * j * j;
-			long long root = (long long)round(sqrt((double)sum));
-			if (root * root == sum) {
-				cout << i << ", " << j << ", " << root << endl;
-				_getch();
-				return 0;
-			}
+	// BRUTEFORCE
+	/*
+	for (int a = start; a < stop + 1; a++) {
+		int aSqr = a * a;
+		for (int b = a; b < stop + 1; b++) {
+			int sum = aSqr + b * b;
+			int c = b;
+			do {
+				if (++c * c == sum) {
+					cout << "W tym przedziale znajduje się trójka pitagorejska:";
+					cout << "a= " << a << ", b=" << b << ", c=" << c << endl;
+					_getch();
+					return 0;
+				}
+			} while (c * c < sum);
 		}
 	}
-	cout << "W tym przedziale nie ma żadnej trójki pitagorejskiej";
+	cout << "W tym przedziale nie znaleziono żadnej trójki pitagorejskiej";
+	*/
+
+	for (int m = 2; m <= floor(sqrt(stop + 1)); m++ ) {
+		int mSqr = m * m;
+		for (int n = 1; n < m; n++) {
+			if (nwd(m, n) != 1 || (n + m) % 2 != 1) break;
+			int a = mSqr - n * n;
+			int b = 2 * m * n;
+			if (a > stop || b > stop) break;
+			int k = 1;
+			while (true) {
+				if (k * a > stop || k * b > stop) break;
+				if (k * a > start-1 && k * b > start-1) {
+					cout << "Znaleziono w tym przedziale trójkę pitagorejską: ";
+					cout << "a= " << a*k << ", b=" << b*k << ", c=" << (mSqr + n*n) * k << endl;
+					_getch();
+					return 0;
+				}
+				k++;
+			}
+		}
+	} 
+	cout << "W tym przedziale nie znaleziono żadnej trójki pitagorejskiej";
 
 
 
-	
+
 	_getch();
 }
