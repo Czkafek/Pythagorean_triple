b52f32d3e12c1718e7bc2e5d508ad91a4bb60863
Czkafek <czkafek@gmail.com>
2025-06-04 15:36:33 +0200

    Project is done

--- Pythagorean_triple.cpp
+++ Pythagorean_triple.cpp
@@ -1,10 +1,9 @@
 ﻿#include <iostream>
 #include <conio.h>
-#include <cmath>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-int nwd(int a, int b) {
+long long nwd(long long a, long long b) {
 	while (a != b)
 	{
 		if (a > b) a -= b;
@@ -12,7 +11,7 @@ int nwd(int a, int b) {
 	}
 	return a;
 }
-
+/*
 class Triple {
 public:
 	int a, b, c;
@@ -23,7 +22,17 @@ public:
 		this->c = c;
 	}
 };
-// Lepiej trzymać w tablicach tablice
+*/
+
+struct Triple {
+	long long a, b, c;
+
+	Triple(long long a, long long b, long long c) {
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
+};
 
 int main()
 {
@@ -58,7 +67,8 @@ int main()
 		cout << "W tym przedziale nie znaleziono żadnej trójki pitagorejskiej";
 	*/
 
-	// EUKLIDES
+	// EUKLIDES - ARRAY + CLASS
+	/*
 	bool isFound = false;
 	Triple foundTriples[10000];
 	int count = 0;
@@ -112,10 +122,59 @@ int main()
 	}
 	else
 		cout << "W tym przedziale nie znaleziono żadnej trójki pitagorejskiej";
+	*/
+
+	// EUKLIDES - STRUCT + VECTOR
+	bool isFound = false;
+	vector<Triple> foundTriples;
+
+	for (long long m = 2; m <= floor(sqrt(stop + 1)); m++) {
+		long long mSqr = m * m;
+		for (long long n = 1; n < m; n++) {
+			if (nwd(m, n) != 1 || (n + m) % 2 != 1) continue;
+			long long a = mSqr - n * n;
+			long long b = 2 * m * n;
+			if (a > stop || b > stop) continue;
+			long long k = 1;
+			while (true) {
+				if (k * a > stop || k * b > stop) break;
+				if (k * a > start - 1 && k * b > start - 1) {	
+					foundTriples.push_back({ k * a, k * b, (mSqr + n * n) * k });
+					isFound = true;
+				}
+				k++;
+			}
+		}
+	}
+	if (isFound) {
+		for (int i = 0; i < foundTriples.size(); i++) {
+			for (int j = 0; j < foundTriples.size() - i - 1; j++) {
+				if (foundTriples[j].a > foundTriples[j + 1].a) {
+					int tmpA = foundTriples[j].a, tmpB = foundTriples[j].b, tmpC = foundTriples[j].c;
+					foundTriples[j].a = foundTriples[j + 1].a;
+					foundTriples[j].b = foundTriples[j + 1].b;
+					foundTriples[j].c = foundTriples[j + 1].c;
+					foundTriples[j + 1].a = tmpA;
+					foundTriples[j + 1].b = tmpB;
+					foundTriples[j + 1].c = tmpC;
+				}
+				else if (foundTriples[j].a == foundTriples[j + 1].a) {
+					if (foundTriples[j].b > foundTriples[j + 1].b) {
+						int tmpB = foundTriples[j].b, tmpC = foundTriples[j].c;
+						foundTriples[j].b = foundTriples[j + 1].b;
+						foundTriples[j].c = foundTriples[j + 1].c;
+						foundTriples[j + 1].b = tmpB;
+						foundTriples[j + 1].c = tmpC;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < foundTriples.size(); i++) {
+			cout << "a=" << foundTriples[i].a << ", b=" << foundTriples[i].b << ", c=" << foundTriples[i].c << endl;
+		}
+	}
+	else
+		cout << "W tym przedziale nie znaleziono żadnej trójki pitagorejskiej";
 	
-
-
-
 	_getch();
 }
-
